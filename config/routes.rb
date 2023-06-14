92413f47a2856c0368429de1cf8347a2333dc6d0
Blanca Rodriguez <blancarmp95@gmail.com>
2023-06-13 20:11:08 -0600

    add challene

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+Restaurant.destroy_all
+
+restaurants = [
+  {
+    name: "PF Changs",
+    address: "Rio Lerma 123",
+    phone_number: "555-1234",
+    category: "chinese"
+  },
+  {
+    name: "Babero",
+    address: "Rio Panuco 234",
+    phone_number: "555-1234",
+    category: "italian"
+  },
+  {
+    name: "Rokai",
+    address: "Rio Tiber 345",
+    phone_number: "555-1234",
+    category: "japanese"
+  },
+  {
+    name: "Em",
+    address: "Rio Mississippi 456",
+    phone_number: "555-1234",
+    category: "french"
+  },
+  {
+    name: "Frituur",
+    address: "654 Maple Street",
+    phone_number: "555-1234",
+    category: "belgian"
+  }
+]
+
+restaurants.each do |restaurant_attrs|
+  Restaurant.create!(restaurant_attrs)
+end
